6256354ba14647939f36a1cfde8d39b7cdbbb780
Chris Niemela <christopher.niemela@ontariotechu.net>
2022-12-12 15:48:05 -0500

    trying to add bootstrap theme:

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 module.exports = 
 {
     //used for connecting to local host when testing
-    "URI":"mongodb://127.0.0.1:27017/VideoGames"
+    //"URI":"mongodb://127.0.0.1:27017/VideoGames"
 
     //used for connecting to mongodb server
-    //"URI":"mongodb+srv://chris:ni227Hg6dAXiNAp@mongodbserver.opeu9gp.mongodb.net/videoGames"
+    "URI":"mongodb+srv://chris:ni227Hg6dAXiNAp@mongodbserver.opeu9gp.mongodb.net/videoGames"
 
 }
